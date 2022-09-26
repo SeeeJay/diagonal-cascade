ab2d549b1aa7306c5f60aa67dbf78b4392fd446c
SeeeJay <113055585+SeeeJay@users.noreply.github.com>
2022-09-26 14:39:56 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    for (let x = 0; x <= 2; x++) {
+        for (let y = 0; y <= x; y++) {
+            led.plot(x, y)
+            basic.pause(100)
+            led.plot(y, x)
+        }
+    }
+    for (let x = 0; x <= 4; x++) {
+        for (let y = 0; y <= x; y++) {
+            led.unplot(x, y)
+            basic.pause(100)
+            led.unplot(y, x)
+        }
+    }
+})
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+	
+})
